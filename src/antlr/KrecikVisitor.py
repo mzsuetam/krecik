fc79f4415e2e41e57bae234df2aa74ae574484a0
Patryk Knapek <patrykknapek@MacBook-Pro-Patryk.local>
2023-04-17 17:30:54 +0200

    zmiany

--- src/antlr/KrecikVisitor.py
+++ src/antlr/KrecikVisitor.py
@@ -51,7 +51,30 @@ class KrecikVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by KrecikParser#expression.
     def visitExpression(self, ctx:KrecikParser.ExpressionContext):
-        return self.visitChildren(ctx)
+        '''
+        '(' SP* expression SP* ')'
+        | unary_operator SP* expression
+        | expression SP* binary_operator SP* expression
+        | function_call
+        | VARIABLE_NAME
+        | literal
+        '''
+        if ctx.getChildCount == 1:
+            return
+        
+        first_child = self.visitChildren(ctx.getChild(0))
+        second_child = self.visitChildren(ctx.getChild(1))
+        if first_child == '(':
+            return second_child
+        if first_child == '+':
+            return second_child
+        if first_child == '-':
+            return -second_child
+        if first_child == 'Ne':
+            return not second_child
+        
+        third_child = self.visitChildren(ctx.getChild(2))
+        return 
 
 
     # Visit a parse tree produced by KrecikParser#function_call.

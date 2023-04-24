c9987ae21c1e0187727aead91aca82f57d85516f
Piotr Karaś <pepe500p@gmail.com>
2023-04-24 20:04:08 +0200

    improved driver

--- src/board/tests/board_examples.py
+++ src/board/tests/board_examples.py
@@ -1,4 +1,4 @@
-from random import randrange
+from random import choice, randrange
 
 from board.board import Board
 from board.krecik import Krecik, Position
@@ -9,24 +9,26 @@ def plains(width: int = 16, height: int = 10) -> Board:
     return Board([[TileType.GRASS] * width] * height)
 
 
-def jebus_cross() -> Board:
-    return Board(
-        [
-            [TileType.ROCKS, TileType.ROCKS, TileType.GRASS, TileType.ROCKS, TileType.ROCKS],
-            [TileType.ROCKS, TileType.ROCKS, TileType.GRASS, TileType.ROCKS, TileType.ROCKS],
-            [TileType.GRASS, TileType.GRASS, TileType.MOUND, TileType.GRASS, TileType.GRASS],
-            [TileType.ROCKS, TileType.ROCKS, TileType.GRASS, TileType.ROCKS, TileType.ROCKS],
-            [TileType.ROCKS, TileType.ROCKS, TileType.GRASS, TileType.ROCKS, TileType.ROCKS],
-        ],
-        krecik=Krecik(position=Position(2, 2)),
-    )
+def jebus_cross(width: int = 7, height: int = 7) -> Board:
+    matrix = []
+    for h in range(height):
+        if h == height // 2:
+            random_row = [TileType.GRASS for _ in range(width)]
+        else:
+            random_row = [
+                TileType.GRASS if w == width // 2 else TileType.ROCKS for w in range(width)
+            ]
+        matrix.append(random_row)
+    krecik_col = width // 2
+    krecik_row = height // 2
+    return Board(matrix, krecik=Krecik(position=Position(krecik_col, krecik_row)))
 
 
 def random(width: int = 16, height: int = 10) -> Board:
-    number_of_choices = len(TileType)
+    choices = [tile_type.value for tile_type in TileType]
     matrix = []
     for _ in range(height):
-        random_row = [TileType(randrange(number_of_choices)) for _ in range(width)]
+        random_row = [TileType(choice(choices)) for _ in range(width)]
         matrix.append(random_row)
     krecik_col = randrange(width - 1)
     krecik_row = randrange(height - 1)
